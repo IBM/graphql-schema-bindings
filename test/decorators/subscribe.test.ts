bc4a067fdb54927e358cc107e2b1bc174997439f
Da Wang <shwangda@cn.ibm.com>
2021-10-13 16:17:37 +0800

    add more info
    Signed-off-by: Guan Jun-Ming <gjunming@gmail.com>

--- test/decorators/subscribe.test.ts
+++ test/decorators/subscribe.test.ts
@@ -2,26 +2,31 @@ import { expect } from "chai";
 import { type, field, subscribe, createSchema } from "../../src";
 import TestServer from "../TestServer";
 
-describe("Test @field", () => {
+/**
+ * Apollo server subcription example: https://github.com/apollographql/docs-examples/blob/main/apollo-server/v3/subscriptions/index.js#L34-L38
+ * Grphql.js subscription resolver example: https://github.com/howtographql/graphql-js/blob/master/src/resolvers/Subscription.js
+ **/
+
+describe("Test @subscribe", () => {
   it.only("should export a field on the type", async () => {
     @type
     class SubscriptionQuery {
       @field(String)
-      @subscribe(() => 'channel')
-      name;
+      @subscribe(() => 'async iterator')
+      numberIncremented;
     }
 
     const server = new TestServer(createSchema(SubscriptionQuery));
     const { body } = await server.query({
       query: `
         {
-          name
+          numberIncremented
         }
       `
     });
     expect(body).to.deep.equal({
       data: {
-        name: null,
+        numberIncremented: null,
       }
     });
   });
